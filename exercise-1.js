7702d781e4dec1479a9d77325f83f4275fbf89fc
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-06-08 13:40:44 +0200

    es fixato

--- exercise-1.js
+++ exercise-1.js
@@ -2,31 +2,10 @@
 class Person {
 
     constructor(firstName, lastName) {
-        this._firstName = firstName
-        this._lastName = lastName
-    }
-    get firstName() {
-        return this._firstName
+        this.firstName = firstName
+        this.lastName = lastName
     }
 
-    set firstName(value) {
-        if (value.length < 4) {
-            alert("Nome troppo corto");
-            return;
-        }
-        this._firstName = value;
-    }
-    get lastName() {
-        return this._lastName
-    }
-
-    set lastName(value) {
-        if (value.length < 3) {
-            alert("Cognome troppo corto");
-            return;
-        }
-        this._lastName = value;
-    }
 }
 
 const developer = new Person('Mario', 'Rossi');
